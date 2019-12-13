15758b7a77061899e6c32cfb46d9c8f8b851a92e
firaja <david.bert@ymail.com>
2019-12-13 12:24:09 +0100

    better documentation

--- src/main/java/dev/firaja/utils/capture4j/EasyCapture.java
+++ src/main/java/dev/firaja/utils/capture4j/EasyCapture.java
@@ -5,7 +5,37 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-
+/**
+ * This annotation allows you to define the return value of a method to be associated with an array of {@link Throwable}s.
+ * <p>
+ * <pre>{@code @EasyCapture(returns=<string representation of the value>" what=<array of class literals of Throwables>)}</pre>
+ * </p>
+ * <p>
+ * The `returns` attribute must contain a valid String representation of the value if casted to the returned type of the
+ * annotated method. If not, a {@link CatchException} is thrown.
+ * </p>
+ * <p>
+ * A String representation is considered valid if
+ *  <ul>
+ *      <li>in case of {@link byte} or {@link Byte} the method {@link Byte#valueOf(String)} does not throw a {@link NumberFormatException}</li>
+ *      <li>in case of {@link int} or {@link Integer} the method {@link Integer#valueOf(String)} does not throw a {@link NumberFormatException}</li>
+ *      <li>in case of {@link float} or {@link Float} the method {@link Float#valueOf(String)} does not throw a {@link NumberFormatException}</li>
+ *      <li>in case of {@link double} or {@link Double} the method {@link Double#valueOf(String)} does not throw a {@link NumberFormatException}</li>
+ *      <li>in case of {@link char} or {@link Character} the method {@link String#charAt(int 0)}} does not throw a {@link StringIndexOutOfBoundsException}</li>
+ *  </ul>
+ *  </p>
+ *  <p>
+ *  For {@link boolean} and {@link Boolean} the String is converted through {@link Boolean#valueOf(String)}.
+ * <p>
+ *  In case of {@link String} the actual representation is always considered valid.
+ * <p>
+ *  Other types are not supported.
+ * </p>
+ *
+ * @author David Bertoldi
+ * @version 0.1.0
+ * @since 0.1.0
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface EasyCapture
--- src/main/java/dev/firaja/utils/capture4j/SystemOutHandler.java
+++ src/main/java/dev/firaja/utils/capture4j/SystemOutHandler.java
@@ -1,18 +1,55 @@
 package dev.firaja.utils.capture4j;
 
+import java.io.PrintStream;
+
+/**
+ * This class defines a simple Handler that prints a message
+ * in the console output.
+ *
+ * @author David Bertoldi
+ * @version 0.1.1
+ * @since 0.1.1
+ */
 public class SystemOutHandler implements Handler<Object>
 {
 
+    /**
+     * See {@link Handler#handle(Throwable)}
+     */
     @Override
     public Object handle(Throwable theException)
     {
-        System.out.println(getMessage(theException));
+        getPrintStream().println(getMessage(theException));
         return null;
     }
 
-    static String getMessage(Throwable theException)
+    /**
+     * Override this method in order to change the message.
+     *
+     * @param theException that has been thrown
+     * @return the message to be printed
+     * @since 0.1.1
+     */
+    protected String getMessage(Throwable theException)
+    {
+        return getDefaultMessage(theException);
+    }
+
+    /**
+     * Override this method in order to change the stream.
+     *
+     * @return the {@link PrintStream} to use for the message.
+     * @since 0.1.1
+     */
+    protected PrintStream getPrintStream()
+    {
+        return System.out;
+    }
+
+    protected static String getDefaultMessage(Throwable theException)
     {
         return "Exception" + theException.getClass().getName() + " is thrown because " + theException.getMessage()
                 + "\nReturn null.";
     }
+
 }
